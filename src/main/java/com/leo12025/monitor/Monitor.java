06a0d3bbd17b022e6b27bab6a347e95d268c10cb
Leo <leo@leodemacbook-pro.local>
2022-08-05 13:57:58 +0800

    feat: 增加事件监听
    增加对玩家加入服务器和退出服务器的监听
    增加默认配置文件的构建

--- src/main/java/com/leo12025/monitor/Monitor.java
+++ src/main/java/com/leo12025/monitor/Monitor.java
@@ -2,15 +2,23 @@ package com.leo12025.monitor;
 
 
 import org.bukkit.Server;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.PluginLogger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.Objects;
 import java.util.logging.Level;
 
-public final class Monitor extends JavaPlugin {
+public final class Monitor extends JavaPlugin implements Listener {
 
     private PluginLoader loader;
     private Server server;
@@ -20,6 +28,7 @@ public final class Monitor extends JavaPlugin {
     private ClassLoader classLoader;
     private File configFile;
     private PluginLogger logger;
+    private FileConfiguration config;
 
 
     @Override
@@ -27,7 +36,7 @@ public final class Monitor extends JavaPlugin {
         // Plugin startup logic
         //System.out.println("[Monitor]插件已启动");
         this.logger = new PluginLogger(this);
-        logger.log(Level.INFO,"插件已启动");
+        logger.log(Level.INFO, "插件已被加载");
         this.loader = getPluginLoader();
         this.server = getServer();
         this.file = getFile();
@@ -36,6 +45,8 @@ public final class Monitor extends JavaPlugin {
         this.classLoader = getClassLoader();
         this.configFile = new File(dataFolder, "config.yml");
         this.logger = new PluginLogger(this);
+        this.config = this.getConfig();
+
     }
 
     @Override
@@ -43,8 +54,41 @@ public final class Monitor extends JavaPlugin {
         // Plugin startup logic
         // System.out.println("[Monitor]插件已启动");
         // this.logger = new PluginLogger(this);
-        logger.log(Level.INFO,"插件已启动");
+        logger.log(Level.INFO, "插件已启动");
+        config.addDefault("boardCastType", "feishu");
+        config.options().copyDefaults(true);
+        saveConfig();
 
+        // Enable our class to check for new players using onPlayerJoin()
+        this.server.getPluginManager().registerEvents(this, this);
+    }
+
+    public void boardCastTo(String message) {
+        if (Objects.equals(getConfig().getString("boardCastType"), "feishu")) {
+            logger.log(Level.INFO, "向 Feishu 服务推送消息: " + message);
+
+            //TODO: 加入飞书推送相关代码
+        }
+    }
+
+    // This method checks for incoming players and sends them a message
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        logger.log(Level.INFO,"玩家 " + player.getName() + "加入了游戏，他的 UUID 为: " + player.getUniqueId()+ " 延迟是: "+player.getPing());
+        //[16:07:03] [Server thread/ERROR]: [com.leo12025.monitor.Monitor] [Monitor] 玩家 Xyo0加入了游戏，他的 UUID 为:18f72caf-aba0-4e2d-a403-6cec9734338f 延迟是:0
+        /*
+        if (config.getBoolean("youAreAwesome")) {
+            player.sendMessage("You are awesome!");
+        } else {
+            player.sendMessage("You are not awesome...");
+        }*/
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        logger.log(Level.INFO,"玩家 " + player.getName() + "离开了游戏，他的 UUID 为: " + player.getUniqueId());
 
     }
 
@@ -52,7 +96,8 @@ public final class Monitor extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
         // System.out.println("[Monitor]插件已卸载");
-        // logger.log(Level.INFO,"插件已卸载");
+        logger.log(Level.INFO, "插件已卸载");
+        // 通知保存插件配置
         saveConfig();
     }
 }
